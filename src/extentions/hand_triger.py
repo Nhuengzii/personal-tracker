437a52f8d94dcef9bc4476d8371090af0a6189c8
mahasachan <mahasachan.sa@ku.th>
2023-09-09 17:22:15 +0700

    handle bug case version-1

--- src/extentions/hand_triger.py
+++ src/extentions/hand_triger.py
@@ -13,13 +13,14 @@ class HandTriggerResult:
         self.flag_start_stop = False
         self.time_start = 0
         self.is_stop = False
+        self.results = None
     
-    def _check_stop(self, time_to_stop=3) -> bool:
+    def _check_stop(self, time_to_stop=4) -> bool:
             duration_time = time.time() - self.time_start
             #print(duration_time)
             if self.total_fingers >= 4:
-                print(time.time() - self.time_start)
-                print("is finger >= 4")
+                #print(time.time() - self.time_start)
+                #print("is finger >= 4")
                 if not self.flag_start_stop:
                     print("Start condition met: Starting process.")
                     self.time_start = time.time()
@@ -27,22 +28,30 @@ class HandTriggerResult:
                 
 
                 elif duration_time >= time_to_stop:
-                    print("Stop condition met: Stopping process.")
+                    #print("Stop condition met: Stopping process.")
                     self.is_stop = not self.is_stop
                     self.flag_start_stop = False
+                    duration_time = time.time() - self.time_start
+                    
 
-            elif self.total_fingers < 4 and self.flag_start_stop:    
+            elif (self.total_fingers < 4 and self.flag_start_stop):
+                print("Stop condition met: Stopping process.")    
                 self.flag_start_stop = False
+                duration_time = 0
+                
             
             return self.is_stop
+    
+    def _comeon_sign(self):
+        pass
 
-    def _find_hands(self, cv_img: MatLike, draw=True):
+    def find_hands(self, cv_img: MatLike, draw=True):
         img_RGB = cv.cvtColor(cv_img, cv.COLOR_BGR2RGB)
-        results = self.hands.process(img_RGB)
+        self.results = self.hands.process(img_RGB)
         fingers = []
 
-        if results.multi_hand_landmarks:
-            for hand_lms in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for hand_lms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mp_draw.draw_landmarks(cv_img, hand_lms, self.mp_hand.HAND_CONNECTIONS)
                 if hand_lms.landmark[self.tiplist[0]].x < hand_lms.landmark[self.tiplist[0]-1].x:
@@ -57,31 +66,14 @@ class HandTriggerResult:
 
                 self.total_fingers = fingers.count(1)
                 self._check_stop()
+        else:
+            self.flag_start_stop = False
+            self.time_start = 0
         return cv_img, self.is_stop
+    
+    
     
     def update_is_stop(self):
         return self.is_stop
-
-    # not working
-    
-    
     
 
-    
-    
-    
-my_hand = HandTriggerResult()
-cap = cv.VideoCapture(0)
-
-while True:
-    success, img = cap.read()
-    img, is_stop = my_hand._find_hands(img)
-    print(is_stop)
-
-    cv.imshow('img', img)
-
-    if cv.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-cv.destroyAllWindows()
