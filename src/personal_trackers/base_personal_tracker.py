9d216b4d52dd8451351596e8758cec128fe172e3
Nhuengzii <32nhueng@gmail.com>
2023-09-09 16:22:56 +0700

    add BaseMetric class to BasePersonalTracker

--- src/personal_trackers/base_personal_tracker.py
+++ src/personal_trackers/base_personal_tracker.py
@@ -6,15 +6,18 @@ from src.embedders import BaseEmbedder
 from src.helpers import draw_bbox, rec_check
 import cv2
 
+from src.metrics.base_metric import BaseMetric, MetricType
+
 class TrackResults:
     def __init__(self, detect_result: DetectorResult, target_idx: int) -> None:
         self.detect_result = detect_result
         self.target_idx = target_idx
 
 class BasePersonalTracker():
-    def __init__(self, detector: BaseDetector, embedder: BaseEmbedder, auto_add_target_features: bool = False, auto_add_target_features_interval: int = 60) -> None:
+    def __init__(self, detector: BaseDetector, embedder: BaseEmbedder, metric: MetricType, auto_add_target_features: bool = False, auto_add_target_features_interval: int = 60) -> None:
         self.detector = detector
         self.embedder = embedder
+        self.metric = BaseMetric(metric)
         self._target_features_pool: list[tuple[MatLike, torch.Tensor]] = []
         self._last_update_time = datetime.now()
         self._last_auto_add_target_features_time = datetime.now()
@@ -26,7 +29,11 @@ class BasePersonalTracker():
         if detect_result is None:
             return None
         detected_features = self.embedder.extract_features(frame, detect_result.bboxes)
-        target_idx = self._max_average_cosine_distance(detected_features)
+        target_features: list[torch.Tensor] = []
+        for _, features in self._target_features_pool:
+            target_features.append(features.tolist())
+        ranks = self.metric.rank(torch.tensor(target_features), detected_features)
+        target_idx = ranks[0]
         if draw_result:
             self.draw_result(frame, TrackResults(detect_result, target_idx))
         if self.auto_add_target_features and self._last_auto_add_target_features_time and (datetime.now() - self._last_auto_add_target_features_time).total_seconds() > self.auto_add_target_features_interval:
@@ -50,19 +57,6 @@ class BasePersonalTracker():
                 draw_bbox(frame, bbox, (0, 255, 0), "Target") 
             else:
                 draw_bbox(frame, bbox, (0, 0, 255))
-            
-    def _max_average_cosine_distance(self, detected_features: torch.Tensor) -> int:
-        if len(detected_features) == 0:
-            return -1
-        max_average_cosine_distance = 0
-        max_average_cosine_distance_index = -1
-        avg_cosine_distance = torch.zeros(detected_features.shape[0])
-        for i, (_, target_feature) in enumerate(self._target_features_pool):
-            avg_cosine_distance += torch.nn.functional.cosine_similarity(target_feature.unsqueeze(0), detected_features)
-        avg_cosine_distance /= len(self._target_features_pool) 
-        max_average_cosine_distance = torch.max(avg_cosine_distance)
-        max_average_cosine_distance_index = torch.argmax(avg_cosine_distance)
-        return int(max_average_cosine_distance_index.item())
     
     def _should_add_target_features(self, track_result: TrackResults) -> bool:
         bboxes = track_result.detect_result.bboxes 
