849613f377584386e5da1ddda9aa3da74c326cdf
Nhuengzii <32nhueng@gmail.com>
2023-09-12 14:44:00 +0700

    add more embedder model

--- src/embedders/available_embedder_models.py
+++ src/embedders/available_embedder_models.py
@@ -3,4 +3,10 @@ from src.models import osnet_ain, osnet
 
 class AvailableEmbedderModels(Enum):
     OSNET_X1_0 = "osnet_x1_0"
+    OSNET_X0_75 = "osnet_x0_75"
+    OSNET_X0_5 = "osnet_x0_5"
+    OSNET_X0_25 = "osnet_x0_25"
     OSNET_AIN_X1_0 = "osnet_ain_x1_0"
+    OSNET_AIN_X0_75 = "osnet_ain_x0_75"
+    OSNET_AIN_X0_5 = "osnet_ain_x0_5"
+    OSNET_AIN_X0_25 = "osnet_ain_x0_25"
