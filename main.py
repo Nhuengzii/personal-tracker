c7dd738020a8c4df8cd7339450ea598eabb1ab2b
mahasachan <mahasachan.sa@ku.th>
2023-09-15 19:30:42 +0700

    update main

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from datetime import time
 import cv2
 from cv2.typing import MatLike
 import argparse
+from personal_tracker.helpers import crop_image_from_bbox
 from playsound import playsound
 import threading
 from personal_tracker import PersonalTracker
 from personal_tracker.helpers import draw_bbox
+from src.extentions.hand_triger import HandTriggerResult
+from personal_tracker.detector import Detector
+import math
 
 class VideoStreamWidget(object):
     def __init__(self, src: str):
@@ -41,18 +45,65 @@ def main(source: str | int, args):
 
 
     tracker = PersonalTracker()
+    detector = Detector()
+    hand_trigger = HandTriggerResult()
     # playsound("./cap.mp3")
-    targets = tracker.get_target_from_camera(cap, 4)
-    for target in targets:
-        tracker._tracker.add_target_features(target[0], target[1])
+    #targets = tracker.get_target_from_camera(cap, 4)
+    #for target in targets:
+    #    tracker._tracker.add_target_features(target[0], target[1])
 
     # cv2.imshow("target", tracker._tracker.show_target_images())
-    print(f"Start tracking with {len(tracker._tracker._target_features_pool)} targets")
+    #print(f"Start tracking with {len(tracker._tracker._target_features_pool)} targets")
+    has_target = None
+    target_person = None
+    list_target = []
     while True:
         ret, frame = cap.read()
+        #small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
         if not ret:
             break
-        result = tracker.track(frame, True)
+
+        
+        detector_result = detector.detect(frame)
+        if detector_result is None:
+            continue
+        
+        
+        
+        min_diff = 10000
+        for bbox in detector_result.bboxes:
+            center_person = hand_trigger.get_center_of_frame(bbox)
+            frame_person = crop_image_from_bbox(frame, bbox)
+            hand_person, hand_lm = hand_trigger.process_hands(frame_person,True)
+            if not hand_trigger.is_detect:
+                target_person = None
+                continue
+            #cv2.imshow("target", frame_person)
+            target_person = hand_trigger.get_center_of_frame(bbox)
+
+            diff = math.sqrt((target_person[0] - center_person[0])**2 + (target_person[1] - center_person[1])**2)
+            if diff < min_diff:
+                min_diff = diff
+                target_bbox = bbox
+            target_img = crop_image_from_bbox(frame, target_bbox)
+            cv2.imshow("target", target_img)
+        if target_person:
+            tracker._tracker.add_target_features(target_img, target_bbox)
+            result = tracker.track(frame, True)
+
+            
+                
+        
+        hand_trigger.show_status(frame)
+                
+
+
+
+        
+
+        
+        
+        
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -70,5 +121,4 @@ if __name__ == "__main__":
         vid_source = int(args.source)
     else:
         vid_source = args.source
-    main(source=vid_source, args=args)
     main(source=vid_source, args=args)
