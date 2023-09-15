c8b5bdc9ae7b6bd80d7acbe00e65a23511827967
Nhuengzii <32nhueng@gmail.com>
2023-09-16 00:29:24 +0700

    rename MetricType enum value

--- personal_tracker/metric/metric_type.py
+++ personal_tracker/metric/metric_type.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 class MetricType(Enum):
-    COSINE_SIMILARITY = 0
-    COSINE_SIMILARITY2 = 1
-    EUCLIDEAN_DISTANCE = 2
-    MAHALANOBIS_DISTANCE = 3
-    CSEM_DISTANCE = 4
+    COSINE_SIMILARITY = "cosine_similarity"
+    COSINE_SIMILARITY2 = "cosine_similarity2"
+    EUCLIDEAN_DISTANCE = "euclidean_distance"
+    MAHALANOBIS_DISTANCE = "mahalanobis_distance"
+    CSEM_DISTANCE = "csem_distance"
