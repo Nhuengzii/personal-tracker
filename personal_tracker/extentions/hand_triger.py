f6120b6bdeb218ca09db20cad1f8e35d39dc8384
mahasachan <mahasachan.sa@ku.th>
2023-09-17 00:21:34 +0700

    update function stop, find_target

--- personal_tracker/extentions/hand_triger.py
+++ personal_tracker/extentions/hand_triger.py
@@ -7,7 +7,7 @@ from personal_tracker.helpers import crop_image_from_bbox
 from personal_tracker.kalman_filter import KalmanFilter
 
 
-class HandTriggerResult:
+class HandTrigger:
     def __init__(self):
         self.mp_hand = mp.solutions.hands
         self.hands = self.mp_hand.Hands()
@@ -21,9 +21,8 @@ class HandTriggerResult:
         self.is_stop = False
         self.is_detect = False
         self.is_beckon = False
-        self.time_floded_down_list = []
-        self.count_floded_down = 0
         self.duration_time = 0
+        self.flag_start_time = False
 
     def _has_results(self):
         return self.results.multi_hand_landmarks is not None
@@ -37,113 +36,113 @@ class HandTriggerResult:
     def check_is_beckon(self):
         return self.is_beckon
     
-    def first_person(self, result:list, cv_img:MatLike):
-        for bbox in result:
-            frame_person = crop_image_from_bbox(cv_img, bbox)
-            #for lm in self.results.multi_hand_landmarks:   
-            self.process_hands(frame_person, True)
-            
-
-    def find_beckon(self, hand_lms):
+    def add_frame(self, frame):
+        if self.storage_frame.__len__() >= self.max_storage_frame:
+            self.storage_frame.pop(0)
+        self.storage_frame.append(frame)
+    
+    def find_beckon(self, multi_hand_lms) -> bool:
         fingers = []
-        tip_x =  hand_lms.landmark[self.tiplist[0]].x
-        mcp_x = hand_lms.landmark[self.mcplist[0]].x
-        fingers = [1 if tip_x < mcp_x else 0]
-
-        for id in range(1, 5):
-            tip_y = hand_lms.landmark[self.tiplist[id]].x
-            mcp_y = hand_lms.landmark[self.mcplist[id - 1]].x
-            fingers.append(1 if tip_y < mcp_y else 0)
+        
+        for hand_lms in multi_hand_lms:
+            tip_x =  hand_lms.landmark[self.tiplist[0]].x
+            mcp_x = hand_lms.landmark[self.mcplist[0]].x
+            fingers = [1 if tip_x < mcp_x else 0]
+        
+            for id in range(1, 5):
+                tip_y = hand_lms.landmark[self.tiplist[id]].y
+                mcp_y = hand_lms.landmark[self.mcplist[id - 1]].y
+                fingers.append(1 if tip_y < mcp_y else 0)
             
-            total_fingers = fingers.count(1)
+        total_fingers = fingers.count(1)
 
-            if total_fingers <= 2:
-                duration_finger_folded_down = 0
-                time_finger_floded_down = time.time()
-                self.time_floded_down_list.append(time_finger_floded_down)
-            else:
-                if len(self.time_floded_down_list) > 0:
-                    duration_finger_folded_down = self.time_floded_down_list[-1] - self.time_floded_down_list[0]
-                    self.time_floded_down_list.clear()
-
-                if duration_finger_folded_down < 0.12:
-                        continue
-                
-                self.count_floded_down += 1
-                print("count_floded_down: ----------------------> ", self.count_floded_down)
-
-            if self.count_floded_down >= 2:
-                self.is_beckon = True
-                self.count_floded_down = 0
-                return self.is_beckon
-            
+        if total_fingers <=  1:
+            flag_is_floded_down = True
+        else:
+            flag_is_floded_down = False
+        return flag_is_floded_down
+        
 
     def get_center_of_frame(self, bbox: Tuple[int, int, int, int]) -> Tuple[int, int]:
-        #filter = KalmanFilter()
         center_x = (bbox[0] + bbox[2]) // 2
         center_y = (bbox[1] + bbox[3]) // 2
         center_coordinates = (center_x, center_y)
         return center_coordinates
              
-
-    def is_hand_target(self, coord_center, width, height, hand_lm, wrist_point) -> bool:
-        x = int(coord_center[0])
-        y = int(coord_center[1])
-
-        x_wrist = int(wrist_point[0] * width)
-        y_wrist = int(wrist_point[1] * height)
-
-        for hand_lms in self.results.multi_hand_landmarks:
-            if hand_lms.landmark[self.tiplist[0]].x < hand_lms.landmark[self.mcplist[0]].x:
-                if x_wrist < x:
-                    return True
-            else:
-                if x_wrist > x:
-                    return True
-
     
-    
-    def find_command_detecting(self, hand_lms):
-        total_fingers = []
+    def find_target(self, multi_hand_lms, now_time) -> bool:
         fingers = []
         fingers_stop = []
         ip_list = [3,6,10,14,19]
+        if self.flag_start_time == False:
+            global last_time_detectedd
+            global duration_time
+            global miss_process
+            duration_time = 0
+            miss_process = 0
+        
+        for hand_lms in multi_hand_lms:
+            if hand_lms.landmark[4].y > hand_lms.landmark[0].y and self.is_detect:
+                fingers_stop.append(1)
 
-        if hand_lms.landmark[4].y > hand_lms.landmark[0].y and self.is_detect:
-            fingers_stop.append(1)
-
-            for id in range(1, 5):
-                tip_x = hand_lms.landmark[self.tiplist[id]].x
-                ip_x = hand_lms.landmark[ip_list[id]].x
-                fingers_stop.append(1 if tip_x < ip_x else 0)
+                for id in range(1, 5):
+                    tip_x = hand_lms.landmark[self.tiplist[id]].x
+                    ip_x = hand_lms.landmark[ip_list[id]].x
+                    fingers_stop.append(1 if tip_x < ip_x else 0)
 
         total_fingers_stop = fingers_stop.count(1)
         if total_fingers_stop >= 5:
             self.is_detect = False
             self.is_beckon = False
             
-
         fingers = [1 if hand_lms.landmark[self.tiplist[0]].y > hand_lms.landmark[self.mcplist[0]].y else 0]
         for id in range(1, 5):
             if id == 2 or id == 3:
                 fingers.append(1 if hand_lms.landmark[self.tiplist[id]].y > hand_lms.landmark[self.mcplist[id - 1]].y else 0)
             else:
                 fingers.append(1 if hand_lms.landmark[self.tiplist[id]].y < hand_lms.landmark[self.mcplist[id - 1]].y else 0)
-
+        
         total_fingers = fingers.count(1)
-        if total_fingers >= 5:
+        print("flag_start_time: ", self.flag_start_time)
+        print("total_fingers: ", total_fingers)
+
+        if total_fingers >= 4:
+            if not self.flag_start_time:
+                self.flag_start_time = True
+                last_time_detectedd = now_time
+            else: 
+                duration_time = now_time - last_time_detectedd
+                print("duration_time: ", duration_time)
+        else:
+            self.flag_start_time = False
+            miss_process += 1
+            last_time_detectedd = now_time
+        
+        if miss_process >= 2:
+            self.flag_start_time = False
+            miss_process = 0
+            duration_time = now_time
+
+        if duration_time >= 3:
+            print("YAyyyy")
+            duration_time = 0
+            self.flag_start_stop = False
+            last_time_detectedd = now_time
             self.is_detect = True
-
+    
         return self.is_detect
+        
 
-    def find_stop(self, hand_lms, time_to_stop=4,):
+
+    def find_stop(self, multi_hand_lms, time_to_stop=2,):
         fingers = []
-        fingers = [1 if hand_lms.landmark[self.tiplist[0]].x < hand_lms.landmark[self.tiplist[0] - 1].x else 0] 
-        for id in range(1, 5):
-            if hand_lms.landmark[self.tiplist[id]].y < hand_lms.landmark[self.tiplist[id] - 2].y:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+        for hand_lms in multi_hand_lms:
+            fingers = [1 if hand_lms.landmark[self.tiplist[0]].x < hand_lms.landmark[self.tiplist[0] - 1].x else 0] 
+            for id in range(1, 5):
+                if hand_lms.landmark[self.tiplist[id]].y < hand_lms.landmark[self.tiplist[id] - 2].y:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
             
         self.total_fingers = fingers.count(1)
         self.duration_time = time.time() - self.time_start
@@ -159,33 +158,24 @@ class HandTriggerResult:
             print("Stop condition met: Stopping process.")
             self.flag_start_stop = False
             self.duration_time = 0
+        return self.is_stop
         
     def process_hands(self, cv_img: MatLike, draw=True):
         img_RGB = cv.cvtColor(cv_img, cv.COLOR_BGR2RGB)
         self.results = self.hands.process(img_RGB)
+        multi_hand_landmarks = self.results.multi_hand_landmarks
         
-        detected_hand_landmarks = []  # Initialize a list to store detected hand landmarks
-
         if not self._has_results():
             self.flag_start_stop = False
             self.time_start = 0
             self.duration_time = 0
-            return cv_img, detected_hand_landmarks  # Return an empty list if no hand landmarks found
+            return multi_hand_landmarks
 
         if draw:
-            for hand_lms in self.results.multi_hand_landmarks:
+            for hand_lms in multi_hand_landmarks:
                 self.mp_draw.draw_landmarks(cv_img, hand_lms, self.mp_hand.HAND_CONNECTIONS)
-                detected_hand_landmarks.append(hand_lms)  # Add detected hand landmarks to the list
-            
-        for hand_lms in self.results.multi_hand_landmarks:
-            is_detect = self.find_command_detecting(hand_lms)
-            if not is_detect:
-                continue
-
-            self.find_stop(hand_lms)
-            #self.find_beckon(hand_lms)
-
-        return cv_img, detected_hand_landmarks
+    
+        return multi_hand_landmarks
     
     
     def show_status(self, cv_img: MatLike):
